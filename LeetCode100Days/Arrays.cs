a8e22a339c9d1b1654ca642c7bedfefb709310a1
Sandeep Mishra <Sandeep Mishra@LAPTOP-GVMLQ4JS>
2022-12-27 14:24:56 +0530

    Add project files.

---
+++ LeetCode100Days/Arrays.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode100Days
+{
+    public class Arrays
+    {
+        static void Main(string[] args)
+        {
+            var arra = new int[] { 1, 2, 3, 4, 5, 6,10,12,4,5,5,56,6,6 };
+            int num = 9;
+            Search(arra, num);
+        }
+        public static void Search(int[] arr , int num) 
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] == num) { Console.WriteLine(arr[i]); break; }    
+            }
+
+        }
+
+        public static void InsertIntoEndUnsortedArray(int[] arr , int num)
+        {
+
+        }
+    }
+}
